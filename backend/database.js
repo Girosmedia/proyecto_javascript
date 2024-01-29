56c9279914cd489d5af48f70e86776aad4a108dd
Pavel Chávez San Martín <pavel.chavez@autofin.cl>
2024-01-29 11:37:35 -0300

    dependencias frontend

--- backend/database.js
+++ backend/database.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 //console.log(process.env.MONGODB_URI);
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then((db) => console.log("DB is connected"))
-  .catch((err) => console.error(err));
+  .then((db) => console.log("Aplicación conectada a MongoDB 📚"))
+  .catch((err) => console.error("No se logró conectar a MongoDB:", err));
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,9 +4,9 @@ const router = Router();
 const Book = require("../models/Book");
 
 router.get("/", async (req, res) => {
-  console.log("get");
   const books = await Book.find();
   res.json(books);
+  console.log("Consulta Libros 📖 OK");
 });
 module.exports = router;
 
@@ -14,12 +14,12 @@ router.post("/", async (req, res) => {
   const { title, author, isbn } = req.body;
   const newBook = new Book({ title, author, isbn });
   await newBook.save();
-  console.log("Libro", newBook);
+  //console.log("Libro", newBook);
   res.json({ message: "Book Saved" });
 });
 
 router.delete("/:id", async (req, res) => {
-  const book = await Book.findByIdAndDelete(req.params.id);
-  console.log(book);
+  await Book.findByIdAndDelete(req.params.id);
+  //console.log(book);
   res.json({ message: "Book Deleted" });
 });
